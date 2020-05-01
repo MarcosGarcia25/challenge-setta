277c91c7848c96b92010b3cc88d0f34c4d292c6e
Marcos Garcia <marcos.mgarcia5@gmail.com>
2020-05-01 14:47:49 -0300

    modal swipe

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,9 +1,15 @@
-import React from 'react';
-import { Text, View, TouchableOpacity } from 'react-native';
+import React, {useState} from 'react';
+import { Text, View, TouchableOpacity, Dimensions } from 'react-native';
+
+const dimensions = Dimensions.get('screen')
+
+import Modal from 'react-native-modal'
 
 import MainStyle from '../../assets/style/main'
 
-function App() {
+function App(props) {
+
+    const [percentage, setPercentage] = useState(0)
 
     return (
         <View style={MainStyle.container}>
@@ -24,6 +30,30 @@ function App() {
                     Inserir dados do usuário
                 </Text>
             </TouchableOpacity>
+
+            <Modal
+                scrollOffset={30}
+                scrollTo={null}
+                isVisible={true}
+                onBackButtonPress={null}
+                onModalShow={null}
+                onModalHide={null}
+                hideModalContentWhileAnimating
+                useNativeDriver
+                swipeDirection={'down'}
+                style={{backgroundColor: 'white'}}
+                propagateSwipe
+                onSwipeComplete={({ swipingDirection }) => console.log(swipingDirection)}
+                onSwipeMove={(percentageShown) => {
+                    console.log(percentageShown)
+                    setPercentage(percentageShown)
+                }}
+                {...props}
+            >
+                <View style={{ height: (dimensions.height-125) - percentage }}>
+                    <Text>I am the modal content!</Text>
+                </View>
+            </Modal>
         </View>
     );
 }
