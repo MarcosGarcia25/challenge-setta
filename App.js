4d58ac99205949063fd27572e2efc60f2f87cf7d
Marcos Garcia <marcos.mgarcia5@gmail.com>
2020-05-04 00:58:56 -0300

    bugfix

--- App.js
+++ App.js
@@ -2,15 +2,14 @@ import React, { useEffect } from 'react';
 import { View, Text } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
-import initDatabase from './src/services/initDatabase.service'
+
 const Stack = createStackNavigator();
 
-import Routes from './src/Routes'
+import Routes from './src/routes'
 
 function App() {
 
   useEffect(() => {
-    initDatabase()
   }, [])
 
   return (
