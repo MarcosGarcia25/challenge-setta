7e0cc8ebdfc3766c55e67d2d29b40cf09567e809
Marcos Garcia <marcos.mgarcia5@gmail.com>
2020-05-04 01:00:25 -0300

    remoçao codigo nao utilizado

--- App.js
+++ App.js
@@ -1,16 +1,7 @@
-import React, { useEffect } from 'react';
-import { View, Text } from 'react-native';
-import { NavigationContainer } from '@react-navigation/native';
-import { createStackNavigator } from '@react-navigation/stack';
-
-const Stack = createStackNavigator();
-
+import React from 'react';
 import Routes from './src/routes'
 
 function App() {
-
-  useEffect(() => {
-  }, [])
 
   return (
     <Routes />
